af7c07035d99b87a21bc4535df5844aa0f4ca354
DamianVogel <rodolfovogel@gmail.com>
2018-09-09 17:21:52 -0300

    Cambios en CSS

--- src/app/componentes/principal/principal.component.ts
+++ src/app/componentes/principal/principal.component.ts
@@ -4,7 +4,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.component.html',
-  styleUrls: ['./principal.component.css']
+  styleUrls: ['./principal.component.css',
+              './css/animate.css',
+              './css/bootstrap.css',
+              './css/icomoon.css',
+              './css/style.css',
+              './css/superfish.css',
+              './sass/style.scss',
+              './sass/bootstrap.scss',
+
+             ]
 })
 export class PrincipalComponent implements OnInit {
  public status: any = {
