291e1a91a61ac23ce19586d2ed2be3cfeebd1693
DamianVogel <rodolfovogel@gmail.com>
2018-09-15 20:35:26 -0300

    Logica de Adivina el numero ok

--- src/app/clases/juego-agilidad.ts
+++ src/app/clases/juego-agilidad.ts
@@ -1,14 +1,21 @@
+import { Operator } from "rxjs";
+
 export class JuegoAgilidad {
 
-    numeroIngresado: number;
-    gano: boolean;
+    numeroRandom1:   number;
+    numeroRandom2:   number;
+    numeroIngresado:    number;
+    operadores=         ["*","/","+","-"];  
+    operador:           string;
+    gano:               boolean;
 
-
-
-
-
-
-
-
+    constructor() {
+        this.operador = this.operadores[(Math.floor(Math.random()*4))];
+        this.numeroRandom1 = (Math.floor(Math.random()*10));
+        this.numeroRandom2 = (Math.floor(Math.random()*10));
+       
+        //console.info("operador: "+this.operador);  
+        //console.info("n1: "+this.numeroIngresado1);  
+    }
 
 }
--- src/app/componentes/agilidad-aritmetica/agilidad-aritmetica.component.ts
+++ src/app/componentes/agilidad-aritmetica/agilidad-aritmetica.component.ts
@@ -3,49 +3,89 @@ import { JuegoAgilidad } from '../../clases/juego-agilidad'
 
 import {Subscription} from "rxjs";
 import {TimerObservable} from "rxjs/observable/TimerObservable";
+
 @Component({
   selector: 'app-agilidad-aritmetica',
   templateUrl: './agilidad-aritmetica.component.html',
   styleUrls: ['./agilidad-aritmetica.component.css']
 })
+
 export class AgilidadAritmeticaComponent implements OnInit {
-   @Output() 
-  enviarJuego :EventEmitter<any>= new EventEmitter<any>();
-  nuevoJuego : JuegoAgilidad;
-  ocultarVerificar: boolean;
-  Tiempo: number;
-  repetidor:any;
-  private subscription: Subscription;
+   
+  @Output() 
+    
+    enviarJuego :EventEmitter<any>= new EventEmitter<any>();
+    nuevoJuego : JuegoAgilidad;
+    ocultarVerificar: boolean;
+    Tiempo: number;
+    repetidor:any;
+    private subscription: Subscription;
+  
   ngOnInit() {
   }
-   constructor() {
-     this.ocultarVerificar=true;
-     this.Tiempo=5; 
-    this.nuevoJuego = new JuegoAgilidad();
+    constructor() {
+    this.ocultarVerificar=true;
     console.info("Inicio agilidad");  
   }
+  
   NuevoJuego() {
-    this.ocultarVerificar=false;
-   this.repetidor = setInterval(()=>{ 
+    
+    this.Tiempo=15; 
+    this.nuevoJuego = new JuegoAgilidad();
+    this.nuevoJuego.gano = false;
+    this.ocultarVerificar= false;
+    this.repetidor = setInterval( ()=>{ 
       
       this.Tiempo--;
       console.log("llego", this.Tiempo);
-      if(this.Tiempo==0 ) {
+      
+        if(this.Tiempo==0 ) {
         clearInterval(this.repetidor);
         this.verificar();
         this.ocultarVerificar=true;
         this.Tiempo=5;
-      }
-      }, 900);
+        }
+      }, 
+    900);
 
   }
-  verificar()
-  {
-    this.ocultarVerificar=false;
-    clearInterval(this.repetidor);
-   
 
-   
+  verificar(){
+    
+    var numeroVerifica : number;
+
+    switch(this.nuevoJuego.operador){
+      
+      case '+':
+        numeroVerifica = this.nuevoJuego.numeroRandom1 + this.nuevoJuego.numeroRandom2;
+        break;
+      case '-':
+        numeroVerifica = this.nuevoJuego.numeroRandom1 - this.nuevoJuego.numeroRandom2;
+        break;
+      case '/':
+        numeroVerifica = this.nuevoJuego.numeroRandom1 / this.nuevoJuego.numeroRandom2;
+        break;
+      case '*':
+        numeroVerifica = this.nuevoJuego.numeroRandom1 * this.nuevoJuego.numeroRandom2;
+        break;
+    }
+    
+  if(numeroVerifica == this.nuevoJuego.numeroIngresado){
+  
+    this.nuevoJuego.gano = true;
+  
+  }else{
+
+    this.nuevoJuego.gano = false;
+
+  }
+
+    console.log(this.nuevoJuego.gano);
+    this.ocultarVerificar=false;
+  
+    
+    
+    clearInterval(this.repetidor);
   }  
 
 }
